4bb3be507987dcd3cb8cb3341920c16447478cc8
IbrahimMulla23 <ibrahimmulla353@gmail.com>
2024-04-02 14:39:26 +0530

    fully updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     $("#switch").click(function(){
-        $("h1, h2, p, h3, h4, h5, card-title, li, #shop, .item, .conatiner, .card, a").toggleClass("textclr");
+        $("h1, h2, p, h3, h4, h5, testadj, li, #shop, .item, .conatiner, .card, a").toggleClass("textclr");
         $("body").toggleClass("bg");
     })
 });
